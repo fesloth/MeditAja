3170389830de00350a7d134be95ebe981ab33bfd
fesloth <sazzaliee@gmail.com>
2023-09-22 21:45:49 +0800

    config tailwind

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  @vite('resources/css/app.css')
+</head>
+<body>
+</body>
+</html>
