c04c8c797a777f1c438382f9a3e7adba036a7b00
fesloth <sazzaliee@gmail.com>
2023-09-28 15:34:15 +0800

    model dan migrasi

--- database/migrations/2023_09_28_070949_create_todo_table.php
+++ database/migrations/2023_09_28_070949_create_todo_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('priority');
+            $table->text('description');
+            $table->string('deadline');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todo');
     }
 };
