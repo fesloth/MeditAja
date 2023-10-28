0dfce58a2c0e4f96a94c3ad75d625977bf611326
fesloth <sazzaliee@gmail.com>
2023-10-28 17:11:54 +0800

    notes

---
+++ database/migrations/2023_10_28_060953_create_notes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->text('category_id')->nullable();
+            $table->unsignedBigInteger('user_id'); 
+            $table->text('category');
+            $table->foreign('user_id')->references('id')->on('users'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
